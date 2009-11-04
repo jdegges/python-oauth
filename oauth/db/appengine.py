01c1cd93d7379e008cb77fbd45412b5fc9f6fd01
Paul Tarjan <paul@paulisageek.com>
2009-11-04 02:18:52 -0800

    moved database to seperate module

---
+++ oauth/db/appengine.py
@@ -0,0 +1,71 @@
+import oauth
+
+from google.appengine.ext import db
+class Token(db.Model):
+    """Auth Token.
+
+       Implemented on Appengine DB, but could be stored anywhere that you have available.
+       Just implement all the methods in another data store of your choice
+    """
+    token = db.TextProperty(required=True)
+    secret = db.StringProperty(required=True)
+    expires_in = db.IntegerProperty()
+    session_handle = db.StringProperty()
+    type = db.StringProperty(required=True)
+    
+    def __init__(self, oauth_token, *args, **kwargs):
+        if oauth_token:
+            kwargs['token'] = oauth_token.key
+            kwargs['secret'] = oauth_token.secret
+            kwargs['expires_in'] = oauth_token.expires_in
+            kwargs['session_handle'] = oauth_token.session_handle
+        db.Model.__init__(self, *args, **kwargs)
+
+    def makeToken(self):
+        return oauth.OAuthToken(self.token, self.secret, self.expires_in, self.session_handle)
+    
+    created = db.DateTimeProperty(auto_now_add=True)
+    modified = db.DateTimeProperty(auto_now=True)
+
+
+class User(db.Model):
+    request_token = db.ReferenceProperty(Token, collection_name="request_users")
+    access_token = db.ReferenceProperty(Token, collection_name="access_users")
+    primary_key = db.StringProperty()
+    type = db.StringProperty()
+
+    def set_access_token(self, access_token):
+        token = Token(access_token, type="access")
+        token.put()
+        self.access_token = token
+    def set_request_token(self, request_token):
+        token = Token(request_token, type="request")
+        token.put()
+        self.request_token = token
+
+    def get_request_token(self):
+        return self.request_token.makeToken()
+    def get_access_token(self):
+        return self.access_token.makeToken()
+    def get_key(self):
+        """Make this non-guessable as it is the login token for this system"""
+        return self.primary_key
+    @staticmethod
+    def get(key):
+        """Given the string from key(), retrive the object from the DB"""
+        r = User.all().filter("primary_key =", key).get()
+        if r :
+            return r
+        raise Exception("Can't find object with key: %s" % key)
+
+    def save(self):
+        """Save this to the DB"""
+        if not self.primary_key:
+            import string
+            import random
+            chars = string.letters + string.digits
+            self.primary_key = ''.join(random.sample(chars, 20))
+        return self.put()
+
+    created = db.DateTimeProperty(auto_now_add=True)
+    modified = db.DateTimeProperty(auto_now=True)
--- oauth/helper.py
+++ oauth/helper.py
@@ -28,78 +28,7 @@ import httplib
 import urllib2
 import time
 import oauth
-
-
-from google.appengine.ext import db
-class Token(db.Model):
-    """Auth Token.
-
-       Implemented on Appengine DB, but could be stored anywhere that you have available.
-       Just implement all the methods in another data store of your choice
-    """
-    token = db.TextProperty(required=True)
-    secret = db.StringProperty(required=True)
-    expires_in = db.IntegerProperty()
-    session_handle = db.StringProperty()
-    type = db.StringProperty(required=True)
-    
-    def __init__(self, oauth_token, *args, **kwargs):
-        if oauth_token:
-            kwargs['token'] = oauth_token.key
-            kwargs['secret'] = oauth_token.secret
-            kwargs['expires_in'] = oauth_token.expires_in
-            kwargs['session_handle'] = oauth_token.session_handle
-        db.Model.__init__(self, *args, **kwargs)
-
-    def makeToken(self):
-        return oauth.OAuthToken(self.token, self.secret, self.expires_in, self.session_handle)
-    
-    created = db.DateTimeProperty(auto_now_add=True)
-    modified = db.DateTimeProperty(auto_now=True)
-
-
-class User(db.Model):
-    request_token = db.ReferenceProperty(Token, collection_name="request_users")
-    access_token = db.ReferenceProperty(Token, collection_name="access_users")
-    primary_key = db.StringProperty()
-    type = db.StringProperty()
-
-    def set_access_token(self, access_token):
-        token = Token(access_token, type="access")
-        token.put()
-        self.access_token = token
-    def set_request_token(self, request_token):
-        token = Token(request_token, type="request")
-        token.put()
-        self.request_token = token
-
-    def get_request_token(self):
-        return self.request_token.makeToken()
-    def get_access_token(self):
-        return self.access_token.makeToken()
-    def get_key(self):
-        """Make this non-guessable as it is the login token for this system"""
-        return self.primary_key
-    @staticmethod
-    def get(key):
-        """Given the string from key(), retrive the object from the DB"""
-        r = User.all().filter("primary_key =", key).get()
-        if r : 
-            return r
-        raise Exception("Can't find object with key: %s" % key)
-    
-    def save(self):
-        """Save this to the DB"""
-        if not self.primary_key:
-            import string
-            import random
-            chars = string.letters + string.digits
-            self.primary_key = ''.join(random.sample(chars, 20))
-        return self.put()
-
-    created = db.DateTimeProperty(auto_now_add=True)
-    modified = db.DateTimeProperty(auto_now=True)
-
+import db.appengine as db
 
 class OAuthClient(oauth.OAuthClient):
     """A client to help simplify the oauth pain"""
@@ -157,15 +86,14 @@ class OAuthClient(oauth.OAuthClient):
         token = self.fetch_token(oauth_request)
         oauth_request = oauth.OAuthRequest.from_token_and_callback(token=token, http_url=self.authorization_url)
       
-        user = User(type=self.type)
+        user = db.User(type=self.type)
         user.set_request_token(token)
         user.save()
-
         return user, oauth_request.to_url()
        
 
     def verify(self, user, token, verifier):
-        if type(user) != User : user = User.get(user)
+        if type(user) != db.User : user = db.User.get(user)
 
         request_token = user.get_request_token()
         assert token == request_token.key
@@ -180,7 +108,7 @@ class OAuthClient(oauth.OAuthClient):
         return True
 
     def refresh(self, user):
-        if type(user) != User : user = User.get(user)
+        if type(user) != db.User : user = db.User.get(user)
 
         access_token = user.get_access_token()
         request = oauth.OAuthRequest.from_consumer_and_token(
@@ -194,7 +122,7 @@ class OAuthClient(oauth.OAuthClient):
         
 
     def fetch(self, url, user, *args, **kwargs):
-        if type(user) != User : user = User.get(user)
+        if type(user) != db.User : user = db.User.get(user)
 
         access_token = user.get_access_token()
         request = oauth.OAuthRequest.from_consumer_and_token(
