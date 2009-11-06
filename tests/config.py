60f0225fa823434e2f6da230cd99095f4eb2f8ec
Paul Tarjan <paul@paulisageek.com>
2009-11-05 22:12:13 -0800

    Added consumer tests

---
+++ tests/config.py
@@ -0,0 +1,22 @@
+"""
+These are my testing keys for all the services. Don't use them, or they will get disabled and then nobody can use them.
+"""
+class yahoo3leg:
+    CONSUMER_KEY = 'dj0yJmk9ZkluWDJlbjJQbVE2JmQ9WVdrOWNXWTRURXBwTm0wbWNHbzlNQS0tJnM9Y29uc3VtZXJzZWNyZXQmeD1jNA--'
+    CONSUMER_SECRET = '72790e933316c39c29c81438d78575b50d61a4c4'
+
+class yahoo2leg:
+    CONSUMER_KEY = 'dj0yJmk9aE9INGFvY3N6SXpYJmQ9WVdrOU9VMUhZVEZUTlRBbWNHbzlNQS0tJnM9Y29uc3VtZXJzZWNyZXQmeD1iNw--'
+    CONSUMER_SECRET = 'b0452b9380d48d10a2cbcb741344cea9c5f49408'
+
+class twitter:
+    CONSUMER_KEY = 'pawFyzXYRtrkLbW3CoYA'
+    CONSUMER_SECRET = 'krgRy8oRg2p9zEk4015oYsegy43fNpEDqM7bSXRAeM'
+
+class myspace:
+    CONSUMER_KEY = 'b6d075aae92f4f6daf029bbf9656c18a'
+    CONSUMER_SECRET = '1a87dc704ebc40c2a3225f2c3fbd015b4931a6415cb449daacc97ceab7bf9493'
+
+class google:
+    CONSUMER_KEY = 'python-oauth.paulisageek.com'
+    CONSUMER_SECRET = 'cp0nRYvzDEpmtWpSGacS8qwf'
